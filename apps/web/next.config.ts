733851d7eb582407d5e34df608933fc23029fc39
louis <la.programmations@gmail.com>
2026-01-25 19:28:20 +0100

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@testextension/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
